4f5656b637d09f01b2e0bd2a10ecddd9d7d0d133
SankarPadaveti <sankarpadaveti789@gmail.com>
2024-01-23 16:36:00 +0530

    First COmmit

---
+++ FunctionalPrograming.java
@@ -0,0 +1,11 @@
+
+
+import java.util.List;
+import java.util.Arrays;
+
+public class FunctionalPrograming
+{
+	public static void main(String[] args){
+		List<String> words = Arrays.asList("Apple","Banana");
+	}
+}
